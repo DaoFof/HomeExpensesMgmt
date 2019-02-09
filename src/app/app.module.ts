25befcca9eccab33d5e084f98721fcfdb92c2a48
DaoFof <daofof0gmail.com>
2019-02-09 15:46:36 +0530

    Category template done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {FormsModule} from'@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,6 +26,7 @@ import {MessageService} from 'primeng/api';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     ChartModule,
     ToastModule,
     AppRoutingModule
--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-
+import { trigger, style, transition, animate, keyframes, query, stagger} from '@angular/animations';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
+  animations:[
+    trigger('categories',[
+      transition('* => *',[
+        query(':enter', style({opacity: 0}), {optional:true}),
+        query(':enter',stagger('300ms',[
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))]),{optional:true}),
+          query(':leave',stagger('300ms',[
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+          ]))]),{optional:true})
+      ])
+    ])
+  ]
 })
 export class CategoryComponent implements OnInit {
-
+  categoryCount: number;
+  categoryText:string;
+  categories = ['Rent', 'Petrol', 'CNG'];
   constructor() { }
 
   ngOnInit() {
+    this.categoryCount = this.categories.length;
+  }
+
+  addCategory(){
+    this.categories.push(this.categoryText);
+    this.categoryText = '';
+    this.categoryCount = this.categories.length;
+  }
+  removeCategory(i){
+    this.categories.splice(i, 1);
+    this.categoryCount = this.categories.length;
   }
 
 }
