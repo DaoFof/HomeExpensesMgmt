c6bd18feb193cd9e19648c84a4e6437474c87aa3
DaoFof <daofof0gmail.com>
2019-02-09 12:08:38 +0530

    Integrating widget and primeng chart and toast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +9,10 @@ import { CategoryComponent } from './category/category.component';
 import { IncomeComponent } from './income/income.component';
 import { ExpenseComponent } from './expense/expense.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import {ChartModule} from 'primeng/chart';
+import {ToastModule} from 'primeng/toast';
+import {MessageService} from 'primeng/api';
 
 @NgModule({
   declarations: [
@@ -18,9 +24,12 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    ChartModule,
+    ToastModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {MessageService} from 'primeng/api';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,7 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  data: any;
+
+  constructor(private messageService: MessageService) {
+    this.data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'Income',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: true,
+                borderColor: '#1ab394'
+            },
+            {
+                label: 'Expense',
+                data: [28, 48, 40, 19, 86, 27, 90],
+                fill: true,
+                borderColor: '#ed5565'
+            }
+        ]
+    }
+}
+
+selectData(event) {
+    this.clear()
+    this.messageService.add({severity: 'success', summary: 'Data Selected', 'detail': this.data.datasets[event.element._datasetIndex].data[event.element._index]});
+}
+clear() {
+  this.messageService.clear();
+}
 
   ngOnInit() {
   }
